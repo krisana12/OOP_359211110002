b45d19f1cfe2acb368d2af2e82f0ff59503796c7
krisana12 <krisana.luangsunthong1@gmail.com>
2017-11-17 10:52:13 +0700

    Initial commit

---
+++ src/oop_lab/s359211110002/rmutsv/com/myFirstJAVA.java
@@ -0,0 +1,7 @@
+package oop_lab.s359211110002.rmutsv.com;
+
+public class myFirstJAVA {
+    public static void main(String[] args) {
+        System.out.println("Hello Wordl,I'm SKY");
+    }//main
+}// class
