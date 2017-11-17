575173b72f7594637c4795a29610eef673be8118
krisana12 <krisana.luangsunthong1@gmail.com>
2017-11-17 11:14:06 +0700

    Successfoly information data

--- src/oop_lab/s359211110002/rmutsv/com/myFirstJAVA.java
+++ src/oop_lab/s359211110002/rmutsv/com/myFirstJAVA.java
@@ -3,5 +3,8 @@ package oop_lab.s359211110002.rmutsv.com;
 public class myFirstJAVA {
     public static void main(String[] args) {
         System.out.println("Hello Wordl,I'm SKY");
+        System.out.println("Information System");
+        System.out.println("Faculty of Managemant Technololgy");
+        System.out.println("RMUTSV");
     }//main
 }// class
