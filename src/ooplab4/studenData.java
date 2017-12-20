5a98a40be354d310865487034cffb0d949798841
LAB_COM-MT_1 <krisana12>
2017-12-20 17:48:46 +0700

    OverLoadMethod

---
+++ src/ooplab4/studenData.java
@@ -0,0 +1,18 @@
+package ooplab4;
+//1. ชื่อ-สกุล
+//2. รหัสนักศึกษา
+//3. สาขาที่เรียน และคณะ
+//4. ที่อยู่
+//5. email
+//หมายเหตุ: ต้องรับ่าข้อมูลแต่ละข้อมูลโดยการใช้ methods
+
+public class studenData {
+
+    }
+    public static void main (String[] args){
+        getName();
+    }//main
+
+    private static void getName() {
+    }
+}//class
