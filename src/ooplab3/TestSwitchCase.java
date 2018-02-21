bd697c3dbb75527dfb8440c32c07e4205da44515
krisana12 <krisana.luangsunthong19@gmail.com>
2018-02-22 02:19:03 +0700

    TestSwitchCase

---
+++ src/ooplab3/TestSwitchCase.java
@@ -0,0 +1,16 @@
+package ooplab3;
+
+public class TestSwitchCase {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your number");
+
+        int number = scanner.nextInt();
+        switch (number) {
+            case 1: System.out.println("Your ectered 1.");break;
+            case 2: System.out.println("Your ectered 2.");break;
+            case 3: System.out.println("Your ectered 3.");break;
+            default: System.out.println("Pleae enter number 1-3.");
+        }//switch
+    }
+}
